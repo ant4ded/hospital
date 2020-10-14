9d87e8dc00e52db228935deb3a7dc1c010faeeff
ant4ded <ant4ded@gmail.com>
2020-10-14 15:53:08 +0300

    Refactoring

--- src/main/java/by/epam/hospital/dao/impl/UserDaoImpl.java
+++ src/main/java/by/epam/hospital/dao/impl/UserDaoImpl.java
@@ -4,6 +4,7 @@ import by.epam.hospital.connection.ConnectionException;
 import by.epam.hospital.connection.ConnectionUtil;
 import by.epam.hospital.connection.DataSourceFactory;
 import by.epam.hospital.dao.DaoException;
+import by.epam.hospital.dao.RoleDao;
 import by.epam.hospital.dao.UserDao;
 import by.epam.hospital.entity.Role;
 import by.epam.hospital.entity.User;
@@ -25,37 +26,9 @@ public class UserDaoImpl implements UserDao {
     private static final String SQL_FIND_ROLES = "SELECT title FROM hospital.roles " +
             "INNER JOIN users_roles ON roles.id = users_roles.role_id " +
             "INNER JOIN users ON users_roles.user_id = users.id WHERE users.id = ?";
-    private static final String SQL_FIND_ROLE_ID = "SELECT id FROM roles WHERE title = ?";
     private static final String SQL_UPDATE = "UPDATE users SET login = ?, password = ? WHERE id = ?";
 
-    private int findRoleId(Role role) throws DaoException {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        int id;
-        try {
-            connection = DataSourceFactory.createMysqlDataSource().getConnection();
-            statement = connection.prepareStatement(SQL_FIND_ROLE_ID);
-
-            statement.setString(1, role.name());
-
-            statement.execute();
-            resultSet = statement.getResultSet();
-            if (resultSet.next()) {
-                id = resultSet.getInt(1);
-            } else {
-                throw new DaoException("Can not find id on roles table");
-            }
-
-        } catch (ConnectionException e) {
-            throw new DaoException("Can not create data source", e);
-        } catch (SQLException e) {
-            throw new DaoException("Can not find id on roles table");
-        } finally {
-            ConnectionUtil.closeConnection(connection, statement, resultSet);
-        }
-        return id;
-    }
+    private final RoleDao roleDao = new RoleDaoImpl();
 
     @Override
     public void create(User user) throws DaoException {
@@ -74,7 +47,7 @@ public class UserDaoImpl implements UserDao {
 
             statement = connection.prepareStatement(SQL_CREATE_USER_ROLES);
             statement.setInt(1, userFromDb.getId());
-            statement.setInt(2, findRoleId(Role.CLIENT));
+            statement.setInt(2, roleDao.findRoleId(Role.CLIENT));
             statement.execute();
             statement.close();
         } catch (ConnectionException e) {
