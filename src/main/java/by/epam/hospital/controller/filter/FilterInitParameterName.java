f6b1ebd1284579fbbb72b3b8cf7d968604eeaf64
ant4ded <ant4ded@gmail.com>
2020-10-18 19:46:22 +0300

    Make field ANONYMOUS_USER public

--- src/main/java/by/epam/hospital/controller/filter/FilterInitParameterName.java
+++ src/main/java/by/epam/hospital/controller/filter/FilterInitParameterName.java
@@ -1,7 +1,7 @@
 package by.epam.hospital.controller.filter;
 
 public class FilterInitParameterName {
-    static final String ANONYMOUS_USER = "*anonymous_user*";
+    public static final String ANONYMOUS_USER = "*anonymous_user*";
     static final String ROLE = "role";
 
     private FilterInitParameterName() {
