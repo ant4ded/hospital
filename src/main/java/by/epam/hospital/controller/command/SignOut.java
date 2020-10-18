be62ebf422d1fc68ffb1b3c07d0fdb55864d52e3
ant4ded <ant4ded@gmail.com>
2020-10-18 19:16:04 +0300

    Delete useless exception in method signature

--- src/main/java/by/epam/hospital/controller/command/SignOut.java
+++ src/main/java/by/epam/hospital/controller/command/SignOut.java
@@ -4,14 +4,13 @@ import by.epam.hospital.controller.Command;
 import by.epam.hospital.controller.HospitalUrl;
 import by.epam.hospital.controller.ParameterName;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SignOut implements Command {
     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.getSession().removeAttribute(ParameterName.LOGIN_USERNAME);
         request.getSession().removeAttribute(ParameterName.LOGIN_ROLES);
         response.sendRedirect(HospitalUrl.MAIN_URL);
