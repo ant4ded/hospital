4679b79899658b3f95e77ddebc5cb20e4fda7579
ant4ded <ant4ded@gmail.com>
2020-10-24 19:02:53 +0300

    Added filters for all admin head commands

--- src/main/java/by/epam/hospital/controller/filter/CorrectLoginFilter.java
+++ src/main/java/by/epam/hospital/controller/filter/CorrectLoginFilter.java
@@ -1,13 +1,11 @@
 package by.epam.hospital.controller.filter;
 
 
-import by.epam.hospital.controller.HospitalUrl;
 import by.epam.hospital.controller.ParameterName;
 import by.epam.hospital.controller.filter.validator.UserValidator;
 import by.epam.hospital.entity.table.UsersFieldName;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.StringJoiner;
 
@@ -16,9 +14,8 @@ public class CorrectLoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
+            throws ServletException, IOException {
         boolean isHaveInvalidFields = false;
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         StringJoiner response = new StringJoiner(FilterMessageParameter.DELIMITER,
                 FilterMessageParameter.PREFIX, FilterMessageParameter.SUFFIX);
 
@@ -29,9 +26,8 @@ public class CorrectLoginFilter implements Filter {
         }
         if (isHaveInvalidFields) {
             servletRequest.setAttribute(ParameterName.MESSAGE, response.toString());
-            servletRequest.getRequestDispatcher(httpServletRequest.getRequestURI()
-                    .replace(HospitalUrl.APP_NAME_URL, HospitalUrl.EMPTY))
-                    .forward(servletRequest, servletResponse);
+            servletRequest.getRequestDispatcher(servletRequest
+                    .getParameter(ParameterName.PAGE_OF_DEPARTURE)).forward(servletRequest, servletResponse);
             return;
         }
         filterChain.doFilter(servletRequest, servletResponse);
--- src/main/java/by/epam/hospital/controller/filter/admin/head/CorrectDepartmentFilter.java
+++ src/main/java/by/epam/hospital/controller/filter/admin/head/CorrectDepartmentFilter.java
@@ -1,31 +1,20 @@
 package by.epam.hospital.controller.filter.admin.head;
 
-import by.epam.hospital.controller.HospitalUrl;
 import by.epam.hospital.controller.ParameterName;
 import by.epam.hospital.controller.filter.FilterMessageParameter;
-import by.epam.hospital.controller.filter.validator.UserValidator;
 import by.epam.hospital.entity.Department;
-import by.epam.hospital.entity.table.UsersFieldName;
 
 import javax.servlet.*;
 import java.io.IOException;
 import java.util.StringJoiner;
 
-public class MoveDoctorToDepartmentFilter implements Filter {
-    private final UserValidator userValidator = new UserValidator();
-
+public class CorrectDepartmentFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         boolean isHaveInvalidFields = false;
         StringJoiner response = new StringJoiner(FilterMessageParameter.DELIMITER,
                 FilterMessageParameter.PREFIX, FilterMessageParameter.SUFFIX);
-
-        if (!userValidator.isValidLogin(servletRequest
-                .getParameter(UsersFieldName.LOGIN))) {
-            isHaveInvalidFields = true;
-            response.add(UsersFieldName.LOGIN);
-        }
 
         String parameter = servletRequest.getParameter(ParameterName.DEPARTMENT);
         int i = 0;
@@ -41,7 +30,8 @@ public class MoveDoctorToDepartmentFilter implements Filter {
 
         if (isHaveInvalidFields) {
             servletRequest.setAttribute(ParameterName.MESSAGE, response.toString());
-            servletRequest.getRequestDispatcher(HospitalUrl.PAGE_ROLE_CONTROL).forward(servletRequest, servletResponse);
+            servletRequest.getRequestDispatcher(servletRequest
+                    .getParameter(ParameterName.PAGE_OF_DEPARTURE)).forward(servletRequest, servletResponse);
             return;
         }
         filterChain.doFilter(servletRequest, servletResponse);
