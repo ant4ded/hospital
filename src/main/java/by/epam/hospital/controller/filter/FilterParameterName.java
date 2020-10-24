02bdf932f279a7f43e87bd840d163f80601bf795
ant4ded <ant4ded@gmail.com>
2020-10-24 16:37:23 +0300

    Renamed class with constants and added new constants to it

--- src/main/java/by/epam/hospital/controller/filter/FilterParameterName.java
+++ src/main/java/by/epam/hospital/controller/filter/FilterParameterName.java
@@ -1,9 +1,13 @@
 package by.epam.hospital.controller.filter;
 
-public class FilterInitParameterName {
-    public static final String ANONYMOUS_USER = "*anonymous_user*";
-    static final String ROLE = "role";
+public class FilterParameterName {
+    public static final String PREFIX = "Invalid data. [";
+    public static final String SUFFIX = "]";
+    public static final String DELIMITER = ", ";
 
-    private FilterInitParameterName() {
+    public static final String ANONYMOUS_USER = "*anonymous_user*";
+    public static final String ROLE = "role";
+
+    private FilterParameterName() {
     }
 }
