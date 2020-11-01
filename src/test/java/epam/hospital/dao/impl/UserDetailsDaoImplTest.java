0f75d3693611b05e679cf192096424b2c55cb635
ant4ded <ant4ded@gmail.com>
2020-11-01 04:08:03 +0300

    Added tests

--- src/test/java/epam/hospital/dao/impl/UserDetailsDaoImplTest.java
+++ src/test/java/epam/hospital/dao/impl/UserDetailsDaoImplTest.java
@@ -13,6 +13,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Optional;
+
 @Test(groups = {"dao", "UserDetailsDaoImplTest"}, dependsOnGroups = "UserDaoImplTest")
 public class UserDetailsDaoImplTest {
     private UserDetailsDao userDetailsDao;
@@ -27,7 +29,23 @@ public class UserDetailsDaoImplTest {
     }
 
     @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser")
-    public void create_find_update_findByRegistrationData(User user) throws DaoException {
+    public void create_correctCreate_true(User user) throws DaoException {
+        user.getUserDetails().setUserId(userDao.create(user));
+
+        boolean result = userDetailsDao.create(user.getUserDetails());
+        cleaner.delete(user);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser", expectedExceptions = DaoException.class)
+    public void create_nonExistingUser_exception(User user) throws DaoException {
+        userDetailsDao.create(user.getUserDetails());
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser",
+            dependsOnMethods = "create_correctCreate_true")
+    public void update_correctUpdate_true(User user) throws DaoException {
         UserDetails newUserDetails = new UserDetails();
         newUserDetails.setPassportId(user.getUserDetails().getPassportId());
         newUserDetails.setGender(UserDetails.Gender.MALE);
@@ -38,29 +56,57 @@ public class UserDetailsDaoImplTest {
         newUserDetails.setAddress(user.getUserDetails().getAddress());
         newUserDetails.setPhone(user.getUserDetails().getPhone());
 
-        int userId = userDao.create(user);
-        newUserDetails.setUserId(userId);
-        user.getUserDetails().setUserId(userId);
-        if (!userDetailsDao.create(user.getUserDetails())) {
-            Assert.fail("Create user details failed.");
-        }
-
-        UserDetails userDetailsFindByRegistrationData = userDetailsDao.findByRegistrationData(
-                user.getUserDetails().getFirstName(), user.getUserDetails().getSurname(),
-                user.getUserDetails().getLastName(), user.getUserDetails().getBirthday())
-                .orElseGet(UserDetails::new);
-
-        if (!user.getUserDetails().equals(userDetailsFindByRegistrationData)) {
-            Assert.fail("FindByRegistrationData failed.");
-        }
-
-        userDetailsDao.update(userDetailsFindByRegistrationData, newUserDetails);
-        UserDetails userDetailsFindByUserId = userDetailsDao.findByUserId(user.getUserDetails().getUserId())
-                .orElseGet(UserDetails::new);
-        if (!newUserDetails.equals(userDetailsFindByUserId)){
-            Assert.fail("FindByUserId failed.");
-        }
-
+        user.getUserDetails().setUserId(userDao.create(user));
+        newUserDetails.setUserId(user.getUserDetails().getUserId());
+        UserDetails updatedUserDetails = userDetailsDao.update(user.getUserDetails(), newUserDetails);
         cleaner.delete(user);
+
+        Assert.assertEquals(newUserDetails, updatedUserDetails);
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser")
+    public void update_nonExistentUserId_exception(User user) throws DaoException {
+        Assert.assertEquals(userDetailsDao.update(user.getUserDetails(), user.getUserDetails()), user.getUserDetails());
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser",
+            dependsOnMethods = "create_correctCreate_true")
+    public void findByUserId_correctFind_userDetailsPresent(User user) throws DaoException {
+        user.getUserDetails().setUserId(userDao.create(user));
+
+        userDetailsDao.create(user.getUserDetails());
+        Optional<UserDetails> userDetails = userDetailsDao.findByUserId(user.getUserDetails().getUserId());
+        cleaner.delete(user);
+
+        Assert.assertTrue(userDetails.isPresent());
+    }
+
+    @Test
+    public void findByUserId_nonExistingId_userDetailsEmpty() throws DaoException {
+        Assert.assertTrue(userDetailsDao.findByUserId(0).isEmpty());
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser",
+            dependsOnMethods = "create_correctCreate_true")
+    public void findByRegistrationData_correctFind_userDetailsPresent(User user) throws DaoException {
+        user.getUserDetails().setUserId(userDao.create(user));
+        UserDetails userDetails = user.getUserDetails();
+
+        userDetailsDao.create(user.getUserDetails());
+        Optional<UserDetails> optionalUserDetails = userDetailsDao.findByRegistrationData(userDetails.getFirstName(),
+                userDetails.getSurname(), userDetails.getLastName(), userDetails.getBirthday());
+        cleaner.delete(user);
+
+        Assert.assertTrue(optionalUserDetails.isPresent());
+    }
+
+    @Test(dataProviderClass = Provider.class, dataProvider = "getCorrectUser")
+    public void findByRegistrationData_nonExistingUserDetails_userDetailsEmpty(User user) throws DaoException {
+        UserDetails userDetails = user.getUserDetails();
+
+        Optional<UserDetails> optionalUserDetails = userDetailsDao.findByRegistrationData(userDetails.getFirstName(),
+                userDetails.getSurname(), userDetails.getLastName(), userDetails.getBirthday());
+
+        Assert.assertTrue(optionalUserDetails.isEmpty());
     }
 }
