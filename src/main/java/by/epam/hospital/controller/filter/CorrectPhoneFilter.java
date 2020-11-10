8ad2c00c66585f7d0ba935a24ab7b52120d07ded
ant4ded <ant4ded@gmail.com>
2020-11-10 20:34:11 +0300

    Added filters

--- src/main/java/by/epam/hospital/controller/filter/CorrectPhoneFilter.java
+++ src/main/java/by/epam/hospital/controller/filter/CorrectPhoneFilter.java
@@ -1,39 +1,31 @@
-package by.epam.hospital.controller.filter.receptionist;
+package by.epam.hospital.controller.filter;
 
-import by.epam.hospital.controller.HospitalUrl;
 import by.epam.hospital.controller.ParameterName;
-import by.epam.hospital.controller.filter.FilterMessageParameter;
 import by.epam.hospital.controller.filter.validator.UserValidator;
 import by.epam.hospital.entity.table.UsersDetailsFieldName;
-import by.epam.hospital.entity.table.UsersFieldName;
 
 import javax.servlet.*;
 import java.io.IOException;
 import java.util.StringJoiner;
 
-public class RegisterClientFilter implements Filter {
-
+public class CorrectPhoneFilter implements Filter {
     private final UserValidator userValidator = new UserValidator();
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
+            throws ServletException, IOException {
         boolean isHaveInvalidFields = false;
         StringJoiner response = new StringJoiner(FilterMessageParameter.DELIMITER,
                 FilterMessageParameter.PREFIX, FilterMessageParameter.SUFFIX);
-        if (!userValidator.isValidLogin(servletRequest
-                .getParameter(UsersFieldName.LOGIN))) {
+        if (!userValidator.isValidPhone(servletRequest
+                .getParameter(UsersDetailsFieldName.PHONE))) {
             isHaveInvalidFields = true;
-            response.add(UsersFieldName.LOGIN);
-        }
-        if (!userValidator.isValidPassportId(servletRequest
-                .getParameter(UsersDetailsFieldName.PASSPORT_ID))) {
-            isHaveInvalidFields = true;
-            response.add(UsersDetailsFieldName.PASSPORT_ID);
+            response.add(UsersDetailsFieldName.PHONE);
         }
         if (isHaveInvalidFields) {
             servletRequest.setAttribute(ParameterName.MESSAGE, response.toString());
-            servletRequest.getRequestDispatcher(HospitalUrl.PAGE_REGISTRY).forward(servletRequest, servletResponse);
+            servletRequest.getRequestDispatcher(servletRequest
+                    .getParameter(ParameterName.PAGE_OF_DEPARTURE)).forward(servletRequest, servletResponse);
             return;
         }
         filterChain.doFilter(servletRequest, servletResponse);
