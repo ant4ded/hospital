85ea863d797ce19459901003510496bf5f9bd9ea
ant4ded <ant4ded@gmail.com>
2020-11-11 11:42:47 +0300

    Remove non thrown exception

--- src/main/java/by/epam/hospital/controller/command/SignOut.java
+++ src/main/java/by/epam/hospital/controller/command/SignOut.java
@@ -1,18 +1,17 @@
 package by.epam.hospital.controller.command;
 
-import by.epam.hospital.controller.HttpCommand;
 import by.epam.hospital.controller.HospitalUrl;
+import by.epam.hospital.controller.HttpCommand;
 import by.epam.hospital.controller.ParameterName;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SignOut implements HttpCommand {
     @Override
-    public Map<String, Object> execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public Map<String, Object> execute(HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> result = new HashMap<>();
         request.getSession().removeAttribute(ParameterName.LOGIN_USERNAME);
         request.getSession().removeAttribute(ParameterName.LOGIN_ROLES);
