d2e8bbedeb686eb0f61ae3692d7c6fbb9508f2e9
ant4ded <ant4ded@gmail.com>
2020-11-12 11:56:27 +0300

    Rewrite method

--- src/main/java/by/epam/hospital/controller/filter/validator/DataValidator.java
+++ src/main/java/by/epam/hospital/controller/filter/validator/DataValidator.java
@@ -1,6 +1,8 @@
 package by.epam.hospital.controller.filter.validator;
 
+import java.sql.Date;
 import java.time.LocalDate;
+import java.time.Month;
 
 public class DataValidator {
     private static final String PASSPORT_ID_REGEX = "[\\d\\p{L}]{14}";
@@ -8,6 +10,17 @@ public class DataValidator {
     private static final String PHONE_REGEX = "\\d{12}";
     private static final String LOGIN_REGEX = "\\p{Ll}{3,15}_\\p{Ll}_\\p{Ll}{3,15}";
     private static final String ICD_CODE_REGEX = "[\\d-\\p{Lu}]{7}";
+    private static final int YEAR_LENGTH = 4;
+    private static final int MULTIPLICITY_LEAP_YEAR = 4;
+    private static final int MONTH_LENGTH = 2;
+    private static final int DAY_LENGTH = 2;
+    private static final int DEFAULT_DAY = 30;
+    private static final int MAX_DAY = 31;
+    private static final int MAX_AGE = 120;
+    private static final int LEAP_YEAR_DAY = 29;
+    private static final int NON_LEAP_YEAR_DAY = 28;
+
+    private final int minYearBirth = LocalDate.now().getYear() - MAX_AGE;
 
     public boolean isValidLogin(String login) {
         boolean result = false;
@@ -33,50 +46,50 @@ public class DataValidator {
         return result;
     }
 
-    public boolean isValidBirthDate(String s) {
+    public boolean isValidDate(String date) {
         boolean result = false;
+        if (date != null) {
+            int firstDashPosition = date.indexOf('-');
+            int secondDashPosition = date.indexOf('-', firstDashPosition + 1);
 
-        final int YEAR_LENGTH = 4;
-        final int MULTIPLICITY_LEAP_YEAR = 4;
-        final int MONTH_LENGTH = 2;
-        final int DAY_LENGTH = 2;
-        final int MAX_MONTH = 12;
-        final int DEFAULT_DAY = 30;
-        final int MAX_DAY = 31;
-        final int MAX_AGE = 120;
-        final int LEAP_YEAR_DAY = 29;
-        final int NON_LEAP_YEAR_DAY = 28;
-        final int MIN_YEAR_BIRTH = LocalDate.now().getYear() - MAX_AGE;
-
-        if (s != null) {
-            int firstDashPosition = s.indexOf('-');
-            int secondDashPosition = s.indexOf('-', firstDashPosition + 1);
-            int len = s.length();
-
-            boolean isValidYearLength = (secondDashPosition > 0) && (secondDashPosition < len - 1) &&
+            boolean isValidYearLength = (secondDashPosition > 0) && (secondDashPosition < date.length() - 1) &&
                     firstDashPosition == YEAR_LENGTH;
             boolean isValidMonthLength = (secondDashPosition - firstDashPosition > 1 &&
                     secondDashPosition - firstDashPosition <= MONTH_LENGTH + 1);
-            boolean isValidDayLength = (len - secondDashPosition > 1 && len - secondDashPosition <= DAY_LENGTH + 1);
+            boolean isValidDayLength = (date.length() - secondDashPosition > 1 &&
+                    date.length() - secondDashPosition <= DAY_LENGTH + 1);
 
             if (isValidYearLength && isValidMonthLength && isValidDayLength) {
-                int year = Integer.parseInt(s, 0, firstDashPosition, 10);
-                int month = Integer.parseInt(s, firstDashPosition + 1, secondDashPosition, 10);
-                int day = Integer.parseInt(s, secondDashPosition + 1, len, 10);
+                int year = Integer.parseInt(date, 0, firstDashPosition, 10);
+                int month = Integer.parseInt(date, firstDashPosition + 1, secondDashPosition, 10);
+                int day = Integer.parseInt(date, secondDashPosition + 1, date.length(), 10);
 
-                boolean isValidMonth = (month >= 1 && month <= MAX_MONTH);
-                boolean isValidYear = year >= MIN_YEAR_BIRTH && year <= LocalDate.now().getYear();
+                boolean isValidMonth = (month >= Month.JANUARY.getValue() && month <= Month.DECEMBER.getValue());
 
-                boolean isValidDaysInThirtyDayMonth = (month == 4 || month == 6 || month == 9 || month == 11)
-                        && day <= DEFAULT_DAY;
-                boolean isValidDaysInThirtyOneDayMonth = (month == 1 || month == 3 || month == 5 || month == 7 ||
-                        month == 8 || month == 10 || month == 12) && day <= MAX_DAY;
-                boolean isValidDaysInFebruary = month == 2 &&
+                boolean isValidDaysInThirtyDayMonth = (month == Month.APRIL.getValue() ||
+                        month == Month.JUNE.getValue() || month == Month.SEPTEMBER.getValue() ||
+                        month == Month.NOVEMBER.getValue()) && day <= DEFAULT_DAY;
+                boolean isValidDaysInThirtyOneDayMonth = (month == Month.JANUARY.getValue() ||
+                        month == Month.MARCH.getValue() || month == Month.MAY.getValue() ||
+                        month == Month.JULY.getValue() || month == Month.AUGUST.getValue() ||
+                        month == Month.OCTOBER.getValue() ||
+                        month == Month.DECEMBER.getValue()) && day <= MAX_DAY;
+                boolean isValidDaysInFebruary = month == Month.FEBRUARY.getValue() &&
                         (day <= NON_LEAP_YEAR_DAY || (day == LEAP_YEAR_DAY && year % MULTIPLICITY_LEAP_YEAR == 0));
 
-                result = isValidMonth && isValidYear &&
-                        (isValidDaysInThirtyDayMonth || isValidDaysInThirtyOneDayMonth || isValidDaysInFebruary);
+                result = isValidMonth && (isValidDaysInThirtyDayMonth || isValidDaysInThirtyOneDayMonth ||
+                        isValidDaysInFebruary);
             }
+        }
+        return result;
+    }
+
+    public boolean isValidBirthDate(String s) {
+        boolean result = false;
+        if (isValidDate(s)) {
+            Date date = Date.valueOf(s);
+            int year = date.toLocalDate().getYear();
+            result = year >= minYearBirth && year <= LocalDate.now().getYear();
         }
         return result;
     }
