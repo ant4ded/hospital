d41e306461ceafe132161cb0c6a0c300bba2c4a4
ant4ded <ant4ded@gmail.com>
2021-05-02 13:32:14 +0300

    Renamed parameter

--- src/main/java/by/epam/hospital/entity/PageResult.java
+++ src/main/java/by/epam/hospital/entity/PageResult.java
@@ -3,20 +3,20 @@ package by.epam.hospital.entity;
 import java.util.List;
 
 public class PageResult<T> {
-    private final int total;
+    private final int totalPages;
     private final List<T> list;
 
-    private PageResult(List<T> list, int total) {
+    private PageResult(List<T> list, int totalPages) {
         this.list = list;
-        this.total = total;
+        this.totalPages = totalPages;
     }
 
-    public static <T> PageResult<T> from(List<T> list, int total) {
-        return new PageResult<>(list, total);
+    public static <T> PageResult<T> from(List<T> list, int totalPages) {
+        return new PageResult<>(list, totalPages);
     }
 
-    public int getTotal() {
-        return total;
+    public int getTotalPages() {
+        return totalPages;
     }
 
     public List<T> getList() {
